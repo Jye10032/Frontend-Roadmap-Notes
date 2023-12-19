9df58fa12a1faa50c8e0e6158f07110a31d4cc9c
Jye10032 <736891807@qq.com>
2023-12-19 20:39:43 +0800

    注释了module，修改了url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,7 +13,7 @@ const config = {
   favicon: 'img/color.svg',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://jye10032.github.io/docusaurus-website/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
