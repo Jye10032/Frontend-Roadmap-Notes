4fa148a59e3f8b829e3a926174d28044ca03ed3d
Jye10032 <736891807@qq.com>
2023-12-19 20:58:29 +0800

    尝试修改baseurl，删除了14.16版本的node

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,10 +13,10 @@ const config = {
   favicon: 'img/color.svg',
 
   // Set the production url of your site here
-  url: 'https://jye10032.github.io/docusaurus-website/',
+  url: 'https://jye10032.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/docusaurus-website/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
