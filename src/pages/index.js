cb90d61b8f783753d8372f20a9cee20f15ef3754
Yueming Gao <92220633+Jye10032@users.noreply.github.com>
2023-12-19 20:28:18 +0800

    Add files via upload

---
+++ src/pages/index.js
@@ -0,0 +1,58 @@
+import { useState, useEffect } from "react";
+import Layout from '@theme/Layout';
+import styles from './index.module.css';
+
+
+
+export default function Home() {
+    const [loopNum, setLoopNum] = useState(0);
+    const [isDeleting, setIsDeleting] = useState(false);
+    const [text, setText] = useState('');
+    const [delta, setDelta] = useState(300 - Math.random() * 100);
+    const [index, setIndex] = useState(1);
+    const toRotate = ["Web Developer", "Web Designer", "Product Manager"];
+    const period = 2000;
+
+    //设置打字机效果
+    useEffect(() => {
+        let ticker = setInterval(() => {
+            tick();
+        }, delta);
+
+        return () => { clearInterval(ticker) };
+    }, [text])
+
+    const tick = () => {
+        let i = loopNum % toRotate.length;
+        let fullText = toRotate[i];
+        let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
+
+        setText(updatedText);
+
+        if (isDeleting) {
+            setDelta(prevDelta => prevDelta / 2);
+        }
+
+        if (!isDeleting && updatedText === fullText) {
+            setIsDeleting(true);
+            setIndex(prevIndex => prevIndex - 1);
+            setDelta(period);
+        } else if (isDeleting && updatedText === '') {
+            setIsDeleting(false);
+            setLoopNum(loopNum + 1);
+            setIndex(1);
+            setDelta(500);
+        } else {
+            setIndex(prevIndex => prevIndex + 1);
+        }
+    }
+    return (
+        <Layout>
+            <section>
+                <span className={styles.tagline}>Welcome to my Blog</span>
+                <h1>{`Hi! I'm Ym_Gao, `}<br />{`a `}{text}</h1>
+            </section>
+        </Layout>
+
+    );
+}
